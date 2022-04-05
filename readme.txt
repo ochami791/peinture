27dda4cf95fe39d1dce11ae0c6a2bb649e9b95c6
ochami <ochami@ochami.fr>
2022-04-05 13:05:32 +0200

    ajout de bootstrap5 + webpack encore

--- readme.txt
+++ readme.txt
@@ -2,3 +2,20 @@
 
 peinture est un site internet
 
+### Pré-requis
+ php 7.4
+ composer
+ Docker
+ Docker-composer
+
+ ### lancer l'environement de développement
+
+ docker-compose up -d
+ symfony serve -d 
+
+### Lancer des tests
+
+php bin/phpunit --testdoc
+
+
+
---
+++ src/Repository/BlogpostRepository.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Blogpost;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\OptimisticLockException;
+use Doctrine\ORM\ORMException;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @method Blogpost|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Blogpost|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Blogpost[]    findAll()
+ * @method Blogpost[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class BlogpostRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Blogpost::class);
+    }
+
+    /**
+     * @throws ORMException
+     * @throws OptimisticLockException
+     */
+    public function add(Blogpost $entity, bool $flush = true): void
+    {
+        $this->_em->persist($entity);
+        if ($flush) {
+            $this->_em->flush();
+        }
+    }
+
+    /**
+     * @throws ORMException
+     * @throws OptimisticLockException
+     */
+    public function remove(Blogpost $entity, bool $flush = true): void
+    {
+        $this->_em->remove($entity);
+        if ($flush) {
+            $this->_em->flush();
+        }
+    }
+
+    // /**
+    //  * @return Blogpost[] Returns an array of Blogpost objects
+    //  */
+    /*
+    public function findByExampleField($value)
+    {
+        return $this->createQueryBuilder('b')
+            ->andWhere('b.exampleField = :val')
+            ->setParameter('val', $value)
+            ->orderBy('b.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+    */
+
+    /*
+    public function findOneBySomeField($value): ?Blogpost
+    {
+        return $this->createQueryBuilder('b')
+            ->andWhere('b.exampleField = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+    */
+}
